7e64a7eaf8bcb608c9523917fb677ac7f71fd27a
jongik-sv <yoo7032@gamil.com>
2021-01-19 23:15:38 +0900

    progressbar 추가

--- client/src/App.js
+++ client/src/App.js
@@ -7,46 +7,59 @@ import TableHead from "@material-ui/core/TableHead";
 import TableBody from "@material-ui/core/TableBody";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import { withStyles } from "@material-ui/core/styles";
 
 const styles = (theme) => ({
   root: {
     width: "100%",
-    marginTop: theme.spacing.unit * 3,
+    // marginTop: theme.spacing.unit * 3,
     overflowX: "auto",
   },
   table: {
     minWidth: 1080,
   },
+  progress: {
+    margin: theme.spacing.unit * 2,
+  },
 });
 
-const customers = [
-  {
-    id: 1,
-    image: "http://placeimg.com/64/64/1",
-    name: "홍길동",
-    birthday: "911112",
-    gender: "남자",
-    job: "학생",
-  },
-  {
-    id: 2,
-    image: "http://placeimg.com/64/64/2",
-    name: "김길동",
-    birthday: "911122",
-    gender: "여자",
-    job: "파이터",
-  },
-  {
-    id: 3,
-    image: "http://placeimg.com/64/64/3",
-    name: "박찬호",
-    birthday: "921112",
-    gender: "남자",
-    job: "코딩만하는 사람",
-  },
-];
+/* 
+1) constructor()
+
+2) componentWillMount()
+
+3) render()
+
+4) componentDidMount(\
+
+*/
+
+/*
+
+props or state가 변경 => shouldComponentUpdate()
+상태만 잘 관리하면 자동으로 다시 그려준다
+*/
+
 class App extends Component {
+  state = {
+    customers: "",
+    completed: 100,
+  };
+
+  componentDidMount() {
+    this.callApi()
+      .then((res) => this.setState({ customers: res }))
+      .catch((err) => console.error(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch("/api/customers");
+    const body = await response.json();
+    console.log(body);
+    return body;
+  };
+
   render() {
     const { classes } = this.props;
     return (
@@ -63,19 +76,27 @@ class App extends Component {
             </TableRow>
           </TableHead>
           <TableBody>
-            {customers.map((c) => {
-              return (
-                <Customer
-                  key={c.id}
-                  id={c.id}
-                  image={c.image}
-                  name={c.name}
-                  birthday={c.birthday}
-                  gender={c.gender}
-                  job={c.job}
-                />
-              );
-            })}
+            {this.state.customers ? (
+              this.state.customers.map((c) => {
+                return (
+                  <Customer
+                    key={c.id}
+                    id={c.id}
+                    image={c.image}
+                    name={c.name}
+                    birthday={c.birthday}
+                    gender={c.gender}
+                    job={c.job}
+                  />
+                );
+              })
+            ) : (
+              <TableRow>
+                <TableCell colspan="6" align="center">
+                  <CircularProgress />
+                </TableCell>
+              </TableRow>
+            )}
           </TableBody>
         </Table>
       </Paper>
--- server.js
+++ server.js
@@ -10,6 +10,37 @@ app.get("/api/hello/", function (req, res) {
   res.send({ message: "Hello Express!" });
 });
 
+app.get("/api/customers", function (req, res) {
+  setTimeout(function () {
+    res.send([
+      {
+        id: 1,
+        image: "http://placeimg.com/64/64/1",
+        name: "홍길동",
+        birthday: "911112",
+        gender: "남자",
+        job: "학생",
+      },
+      {
+        id: 2,
+        image: "http://placeimg.com/64/64/2",
+        name: "김길동",
+        birthday: "911122",
+        gender: "여자",
+        job: "파이터",
+      },
+      {
+        id: 3,
+        image: "http://placeimg.com/64/64/3",
+        name: "박찬호",
+        birthday: "921112",
+        gender: "남자",
+        job: "코딩만하는 사람",
+      },
+    ]);
+  }, 2000);
+});
+
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
 });
